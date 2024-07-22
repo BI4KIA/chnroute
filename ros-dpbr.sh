6104ccb9234ae3aeba508f0f5f224517cfa7b8fd
BI4KIA <80705560+BI4KIA@users.noreply.github.com>
2024-07-22 10:49:32 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -2,14 +2,14 @@
 mkdir -p ./pbr
 cd ./pbr
 
-# AS4809 BGP
+
 wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
 
 {
 echo "/ip firewall address-list"
 
 for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+  echo "add list=List_ALL_China address=$net comment=CN_list"
 done
 
 } > ../CN.rsc
